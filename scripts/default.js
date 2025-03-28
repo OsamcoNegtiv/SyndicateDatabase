c4aff256d3191457f11d5391de4cb0b32fdbc15b
Osamco Negtiv <osamcoolster101@gmail.com>
2025-03-28 15:05:46 -0500

    Added Character Passwords

--- scripts/default.js
+++ scripts/default.js
@@ -8,8 +8,60 @@ function loginscript() {
         currentModal = "Carson";
         modal = document.getElementById(currentModal);
         displaymodal()
-    } else if (uname == "☐" && pword == "box") {
-      currentModal = "Box";
+    } else if (uname == "thespisExtraordinaire" && pword == "Feld_08d14h!") {
+      currentModal = "Amphibole";
+      modal = document.getElementById(currentModal);
+      displaymodal()
+    } else if (uname == "handdrawnDestruction" && pword == "1amNOT0samco") {
+      currentModal = "Ante";
+      modal = document.getElementById(currentModal);
+      displaymodal()
+    } else if (uname == "ifTrueEqualsFalse" && pword == "1nv3rt") {
+      currentModal = "Cecil";
+      modal = document.getElementById(currentModal);
+      displaymodal()
+    } else if (uname == "DubloonsSansPantaloons" && pword == "Y4rh4rf!ddled33d33") {
+      currentModal = "Danassi";
+      modal = document.getElementById(currentModal);
+      displaymodal()
+    } else if (uname == "Openhimer" && pword == "CARS-ONSK-YPEC-ALL1") {
+      currentModal = "Boomer";
+      modal = document.getElementById(currentModal);
+      displaymodal()
+    } else if (uname == "melancholicEsoterica" && pword == "myf4!rAilla") {
+      currentModal = "Iovites";
+      modal = document.getElementById(currentModal);
+      displaymodal()
+    } else if (uname == "RIGHTFULKING" && pword == "BRINGMEGRAPES") {
+      currentModal = "Lard";
+      modal = document.getElementById(currentModal);
+      displaymodal()
+    } else if (uname == "SoftMobster" && pword == "1nth3w33ds") {
+      currentModal = "Leo";
+      modal = document.getElementById(currentModal);
+      displaymodal()
+    } else if (uname == "Maddox" && pword == "WhereHeShouldntBe") {
+      currentModal = "Maddox";
+      modal = document.getElementById(currentModal);
+      displaymodal()
+    } else if (uname == "DesignerGoat" && pword == "p0rt4l4cc3ss") {
+      currentModal = "Pygmal";
+      modal = document.getElementById(currentModal);
+      displaymodal()
+    } else if (uname == "AllTheLimerick" && pword == "Licini0!") {
+      currentModal = "Sarah";
+      modal = document.getElementById(currentModal);
+      displaymodal()
+    } else if (uname == "ScienceWinsAgain" && pword == "L4bR4ts") {
+      currentModal = "Simon";
+      modal = document.getElementById(currentModal);
+      displaymodal()
+    } else if (uname == "TheBoss" && pword == "DieSiedler-Kosmos-1995") {
+      currentModal = "Boss";
+      modal = document.getElementById(currentModal);
+      displaymodal()
+    } else if (uname == "UwUSempai" && pword == "OwOKohai") {
+      currentModal = "UwUSempai";
       modal = document.getElementById(currentModal);
       displaymodal()
     } else {
